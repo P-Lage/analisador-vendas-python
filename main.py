3cde74c7be1c9ed2a29dcfc9689ed335ccb1428f
Pedro Lage <lagecomg@gmail.com>
2025-01-31 19:10:31 -0300

    Adicionada funcionalidade para a visualização dos dados

--- main.py
+++ main.py
@@ -27,3 +27,19 @@ produto_mais_vendido = total_vendas_por_produto.idxmax()
 total_do_produto_mais_vendido = total_vendas_por_produto.max()
 print(f"\nO produto mais vendido foi: {produto_mais_vendido} com \
 {total_do_produto_mais_vendido} vendas.")
+
+# Criando a parte de visualização dos dados
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(8, 5))
+
+total_vendas_por_produto.plot(kind='bar', color='skyblue')
+
+plt.title('Total de Vendas por Produto', fontsize=16)
+plt.xlabel('Produto', fontsize=12)
+plt.ylabel('Total de Vendas', fontsize=12)
+
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+plt.show()

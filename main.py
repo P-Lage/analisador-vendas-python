ab04b717c3c96e7928fbf7561f8c528b2eac961b
Pedro Lage <lagecomg@gmail.com>
2025-02-01 23:12:26 -0300

    Refatorando o código para lidar com um DataSet

--- main.py
+++ main.py
@@ -1,45 +1,13 @@
 import pandas as pd
 
-# Leitura de dados
+caminho_arquivo = 'Sample - Superstore.csv'
+dados = pd.read_csv(caminho_arquivo)
 
-dados = pd.read_csv('dados_vendas.csv')
+print("Primeiras linhas do dataset:")
 print(dados.head())
 
-# Cálculo do total de vendas
+print("\nInformações do dataset:")
+print(dados.info())
 
-total_vendas = dados['Vendas'].sum()
-print(f"Total de vendas: {total_vendas}")
-
-# Cálculo da média de vendas por produto
-
-media_vendas_por_produto = dados.groupby('Produto')['Vendas'].mean().round(2)
-print("\nMédia de vendas por produto:")
-for produto, media in media_vendas_por_produto.items():
-    print(f"{produto}: {media} vendas/mês")
-
-# Cálculo do total de vendas por produto
-
-total_vendas_por_produto = dados.groupby('Produto')['Vendas'].sum()
-
-# Determinando o produto mais vendido
-
-produto_mais_vendido = total_vendas_por_produto.idxmax()
-total_do_produto_mais_vendido = total_vendas_por_produto.max()
-print(f"\nO produto mais vendido foi: {produto_mais_vendido} com \
-{total_do_produto_mais_vendido} vendas.")
-
-# Criando a parte de visualização dos dados
-
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(8, 5))
-
-total_vendas_por_produto.plot(kind='bar', color='skyblue')
-
-plt.title('Total de Vendas por Produto', fontsize=16)
-plt.xlabel('Produto', fontsize=12)
-plt.ylabel('Total de Vendas', fontsize=12)
-
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-
-plt.show()
+print("\nColunas disponíveis:")
+print(dados.columns)
